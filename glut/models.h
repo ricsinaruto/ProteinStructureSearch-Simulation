8e2e96c9a10d323ff6220dba4490268cceb6be1f
ricsinaruto <ricsinaruto@hotmail.com>
2017-05-01 10:04:50 +0200

    nothing

--- glut/models.h
+++
@@ -1,6 +0,0 @@
-void spike(double x, double y, double z,
-	double r, double h, int deg,
-	double ox, double oy, double oz);
-void tower(double x, double y, double z,
-	double dx, double dy, double dz,
-	double th);
--- glut/textures.h
+++
@@ -1,1 +0,0 @@
-unsigned int loadTexBMP(char* file);
